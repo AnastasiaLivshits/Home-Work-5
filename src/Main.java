67eeeea37d4626b0b35a28eeacecb9b5986fe879
AnastasiaLivshits <nastasia.arch@gmail.com>
2024-08-30 20:58:31 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        System.out.println("Task1");
+        int clientOs = 0;
+        if (clientOs ==0)
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        else
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+    {
+        int deviceYear = 2015;
+        int iOs = 0;
+        int android = 1;
+        if (iOs == 0 && deviceYear < 2015) ;
+        {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if (android == 1 && deviceYear < 2015) ;
+        {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        else if (deviceYear =>2015 && iOs == 0){
+        System.out.println("Установите версию приложения для iOS по ссылке");
+        }else{
+            System.out.println("Установите версию приложения для Android по ссылке");}
+            }};
+
+
+
